1be93267b6c2b26c0c9f6431dde7ca45fa89f368
erokez <parshin.k.07@gmail.com>
2025-12-24 21:38:43 +0300

    updated key regex to include any character and backspace

--- shared/vars/regexes/regexes.go
+++ shared/vars/regexes/regexes.go
@@ -4,5 +4,5 @@ import "regexp"
 
 var (
 	ColorRegex = regexp.MustCompile(`^(#([A-Fa-f0-9]{2,6}|[A-Fa-f0-9]{6})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9]))$`)
-	KeyRegex = regexp.MustCompile(`^((ctrl\+)?(tab\+)?(([a-z])|enter))$`)
+	KeyRegex = regexp.MustCompile(`^((ctrl\+)?(tab\+)?(.|enter|backspace))$`)
 )
