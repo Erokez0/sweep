46d7a762755fb2f30c87fb75bc91341529fc9a1e
erokez <parshin.k.07@gmail.com>
2026-01-03 12:40:28 +0300

    added environment variable keys constants

---
+++ shared/consts/env-keys/env-keys.go
@@ -0,0 +1,10 @@
+package envkeys
+
+import consts "sweep/shared/consts/misc"
+
+const (
+	Preview string = consts.APP_NAME + "_is_theme_preview"
+	Height string = consts.APP_NAME + "_field_height"
+	Width string = consts.APP_NAME + "_field_width"
+	Mines string = consts.APP_NAME + "_mine_count"
+)
