516dbb14afe7d8fdf1c6f16007cbbd0d1913161d
erokez <parshin.k.07@gmail.com>
2026-01-05 02:14:45 +0300

    replaced tabs with spaces in the help message

--- shared/consts/misc/misc.go
+++ shared/consts/misc/misc.go
@@ -8,18 +8,26 @@ const (
 ███████║╚███╔███╔╝███████╗███████╗██║     
 ╚══════╝ ╚══╝╚══╝ ╚══════╝╚══════╝╚═╝`
 	AppName     = "sweep"
-	HelpMessage = "Usage" + AppName + `[OPTION] ...
+	HelpMessage = "Usage " + AppName + ` [OPTION] ...
 List of options:
-	--help 									display help and exit
-	--D, --default-config 	copy the default configuration file to the destination of the main configuration file (config.json) and exit
-	--C, --config-path 			print out the canonical path of the configuration file and exit
-	--P, --preview 					print out a preview of your color theme and exit
+  --help                  display help and exit
+  --D, --default-config   copy the default configuration file to the 
+                            destination of the main configuration file and exit
+--C, --config-path        print out the canonical path of the 
+                            configuration file and exit
+--P, --preview            print out a preview of your color theme and exit
 
-	--A, --ascii 						replace glyphs for tiles (like flags and mines) to ASCII friendly character
-	--F, --fill 						vary how the color affect the tiles. If the option is provided, then the colors will fill the background of the tiles, otherwise they will fill the foreground
+--A, --ascii              replace glyphs for tiles (like flags and mines) 
+                            to ASCII friendly characters
+--F, --fill               vary how the color affect the tiles. If the option 
+                            is provided, then the colors will fill the background of 
+                            the tiles, otherwise they will fill the foreground
 
-	--M, --mines[ uint16]		sets the desired amount of mines to the field if other field arguments are set
-	--W, --width[ uint16]		sets the desired field width in columns if other field arguments are set
-	--H, --height[ uint16]	sets the desired field height in rows if other field arguments are set
-	`
+--M, --mines[ uint16]     sets the desired amount of mines to the field
+                            if other field arguments are set
+--W, --width[ uint16]     sets the desired field width in columns 
+                            if other field arguments are set
+--H, --height[ uint16]    sets the desired field height in rows 
+                            if other field arguments are set
+`
 )
