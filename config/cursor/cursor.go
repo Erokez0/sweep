9ffb9c60d7a8746b4c810d0212720c6729dfaf4f
erokez <parshin.k.07@gmail.com>
2026-01-20 11:53:52 +0300

    updated cursor config error

--- config/cursor/cursor.go
+++ config/cursor/cursor.go
@@ -7,6 +7,12 @@ import (
 	styles "sweep/tui/styles"
 )
 
+const configModule string = "cursor"
+const option string = "color"
+
+const leftHalf = "left"
+const rightHalf = "right"
+
 type Cursor struct {
 	Color     colors.Color `json:"color"`
 	LeftHalf  string       `json:"left half"`
@@ -21,16 +27,20 @@ func (e *CursorHalfTooLongError) Error() string {
 	return fmt.Sprintf("(cursor.%v half) cursor %v half must be one character", e.whichHalf, e.whichHalf)
 }
 
+func (e *CursorHalfTooLongError) Is(target error) bool {
+	return e.Error() == target.Error()
+}
+
 func (c *Cursor) Validate() (bool, []error) {
 	errors := []error{}
 	if !c.Color.IsValid() {
-		errors = append(errors, &colors.InvalidColorError{ConfigModule: "config", Option: "color", Value: c.Color})
+		errors = append(errors, &colors.InvalidColorError{ConfigModule: configModule, Option: option, Value: c.Color})
 	}
 	if len(c.LeftHalf) > 1 {
-		errors = append(errors, &CursorHalfTooLongError{"left"})
+		errors = append(errors, &CursorHalfTooLongError{leftHalf})
 	}
 	if len(c.RightHalf) > 1 {
-		errors = append(errors, &CursorHalfTooLongError{"right"})
+		errors = append(errors, &CursorHalfTooLongError{rightHalf})
 	}
 
 	return len(errors) == 0, errors
